e49948cd663bc390e9cba75caa9eafb66ad5b26b
unknown <heyreza8@gmail.com>
2025-01-23 17:49:28 +0330

    fix category routes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('category')->controller(\App\Http\Controllers\Category\CategoryController::class::class)->group(function () {
+Route::prefix('category')->controller(\App\Http\Controllers\Category\CategoryController::class)->group(function () {
     Route::get('/list', 'index');
     Route::get('/{category}', 'show');
     Route::post('/new', 'store');
